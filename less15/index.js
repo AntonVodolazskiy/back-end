7aa6fa9a2a829befadaa1a38198880fd6416e555
AntonVodolazskiy <38651732+AntonVodolazskiy@users.noreply.github.com>
2018-05-08 11:47:01 +0300

    Update index.js

--- less15/index.js
+++ less15/index.js
@@ -9,11 +9,12 @@ function getSize(){
 
 http.createServer(function(req, res){
  	
- 	if((req.url === "/stream") ||(getSize('demo.txt') > 10)) {
+ 	if((req.url === "/stream") || (getSize('demo.txt') > 10)) {
 		
 		var stream = fs.createReadStream('demo.txt');
 		stream.pipe(res);
 		console.log('stream');
+		
  		};
 
 	 if(req.url === "/file") {	
