d1bc1b5149185f284586bc41d0f3a34b8646c680
AntonVodolazskiy <38651732+AntonVodolazskiy@users.noreply.github.com>
2018-05-15 17:00:06 +0300

    add task 1.17

---
+++ less17/converter.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+var request = require('request');
+var url = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5';
+
+request(url, function(err, res, body) {
+	fs.writeFileSync("index.json", body);
+});
+var arr = fs.readFileSync("index.json");
+var rates = JSON.parse(arr);  
+
+var Converter = function() {
+	this.rateUs = rates[0].buy;
+};
+
+Converter.prototype.roundTwoDecimals = function (amount) {
+	return Math.round(amount * 100) / 100;
+};
+
+Converter.prototype.convertToUa = function (currency) {
+	return this.roundTwoDecimals(currency * this.rateUs);
+};
+
+Converter.prototype.convertToUs = function (currency) {
+	return this.roundTwoDecimals(currency / this.rateUs);
+};
+
+module.exports = Converter;
---
+++ less17/index.js
@@ -0,0 +1,7 @@
+var Converter = require('./converter');
+
+
+var converter = new Converter();
+
+console.log(converter.convertToUa(100));
+//console.log(converter.convertToUs(100));
